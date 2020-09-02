be81ef6b52c1ce1cfdbfe20ce56038828f7ecbb3
Andreas <propiece02@gmail.com>
2020-09-02 09:48:38 +0200

    commit

---
+++ Tester/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            if (File.Exsists("words.txt"))
+            {
+                  String[] words = File.ReadAllLines("words.txt");
+            
+                  Console.WriteLine(words[0]);
+
+            }
+            
+        }
+    }
+}
