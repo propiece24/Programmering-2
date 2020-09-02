8f5e2a64df82db002fa8a6433c345afbe4d62ef4
Andreas <propiece02@gmail.com>
2020-09-02 09:57:22 +0200

    commit

--- Programmering 2 Tester/Tester/Program.cs
+++ Programmering 2 Tester/Tester/Program.cs
@@ -7,6 +7,7 @@ namespace Tester
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello mega faggot");
         }
     }
 }
