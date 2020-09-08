53bf5753274c3bdc7deeb6e1320747ceb38e62b0
Andreas <propiece02@gmail.com>
2020-09-08 11:00:41 +0200

    commit

---
+++ Programmering 2 genomgångar/prog2/FootBall.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace prog2
+{
+    public class Fotball
+    {
+
+      public float x;
+
+      public float y;
+
+      public float z;
+      
+
+    }
+}
---
+++ Programmering 2 genomgångar/prog2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace prog2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          FootBall theBall = new FootBall(); 
+          FootBall theOtherBall = new FootBall();
+
+
+          theBall.x = 100;
+
+          System.Console.WriteLine(theOtherBall.x);
+
+          Console.ReadLine();
+
+        }
+    }
+}
